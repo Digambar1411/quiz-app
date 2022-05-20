b3aecc005af82bab61bf381571b4b08833e4d449
Digambar <deshawaldigambar@gmail.com>
2022-05-20 21:48:39 +0530

    feat : added quiz categories in Database

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -5,10 +5,34 @@ import { v4 as uuid } from "uuid";
  * You can add category of your wish with different attributes
  * */
 
-export const categories = [
+ export const categories = [
   {
     _id: uuid(),
-    categoryName: "TV Show",
-    description: "Quizes based on TV shows",
+    name:"Maths",
+    image:"assets/maths.svg",
+    description:"Quizes based on maths",
   },
+
+  {
+    _id: uuid(),
+    name:"Science",
+    image:"assets/science.svg",
+    description:"Quizes based on science",
+  },
+
+  {
+    _id: uuid(),
+    name:"Physics",
+    image:"assets/physics.svg",
+    description:"Quizes based on physics",
+  },
+
+  {
+    _id: uuid(),
+    name:"Web Developmemt",
+    image:"assets/web.svg",
+    description:"Quizes based on web developmemnt",
+  }
+  
 ];
+
