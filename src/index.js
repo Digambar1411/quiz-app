a1110e020db16ec24898211d0eb29839ebeb63fe
Digambar <deshawaldigambar@gmail.com>
2022-06-09 14:44:23 +0530

    feat : provided question context to App

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
+import { QuestionProvider} from "./frontend/contexts/questionContext"
 
 // Call make Server
 makeServer();
@@ -11,8 +12,10 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
-      </BrowserRouter>
+      <QuestionProvider>
+        <App />
+      </QuestionProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
