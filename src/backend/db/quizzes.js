7db93b43ef5b96debce2dbe3b065d0f7f0901092
Digambar <deshawaldigambar@gmail.com>
2022-06-15 14:58:25 +0530

    feat : changes in quiz DB

--- src/backend/db/quizzes.js
+++ src/backend/db/quizzes.js
@@ -1,64 +1,99 @@
 import { v4 as uuid } from "uuid";
 
-/**
- * Product Database can be added here.
- * You can add products of your wish with different attributes
- * */
-
 export const quizzes = [
   {
     categoryName: "maths",
     question: "question number 1",
-    options: ["narendra","modi","om","birla"],
+    options: [
+      { id: uuid(),text:"narendra"},
+      { id: uuid(),text:"modi"},
+      { id: uuid(),text:"om"},
+      { id: uuid(),text:"birla"}
+    ],
     answer: "om"
   },
 
   {
     categoryName: "maths",
     question: "question number 2",
-    options: ["Season 3", "Season 5", "Season 7", "Season 4"],
+    options: [ 
+      {id:1,text:"Season 3"},  
+      {id:1,text:"Season 5"},  
+      {id:1,text:"Season 7"},  
+      {id:1,text:"Season 4"}
+    ],
     answer: "Season 4",
   },
 
   {
     categoryName: "maths",
     question: "question number 3",
-    options: ["Attachment Issues", "Panic Attack", "Trauma", "Stress"],
+    options: [
+      {id:1,text:"Attachment Issues"}, 
+      {id:2,text:"Panic Attack"}, 
+      {id:3,text:"Trauma"}, 
+      {id:4,text:"Stress"}
+    ],
     answer: "Attachment Issues",
   },
 
   {     
     categoryName: "physics",
         question: "question number 4",
-        options: ["Season 1", "Season 2", "Season 6", "Every Season"],
-        answer: "Every Season"
+        options: [ 
+          {id:1,text:"Season 3"},  
+          {id:2,text:"Season 5"},  
+          {id:3,text:"Season 7"},  
+          {id:4,text:"option 4"}
+        ],
+        answer: "option 4"
   },
 
   {
     categoryName: "physics",
     question: "question number 5",
-    options: ["Season 3", "Season 5", "Season 7", "Season 4"],
+    options: [ 
+      {id:1,text:"Season 3"},  
+      {id:2,text:"Season 5"},  
+      {id:3,text:"Season 7"},  
+      {id:4,text:"Season 4"}
+    ],
     answer: "Season 4"
   },
 
   {
     categoryName: "physics",
     question: "question number 6",
-    options: ["Attachment Issues", "Panic Attack", "Trauma", "Stress"],
+    options: [
+      {id:1,text:"Attachment Issues"}, 
+      {id:2,text:"Panic Attack"}, 
+      {id:3,text:"Trauma"}, 
+      {id:4,text:"Stress"}
+    ],
     answer: "Attachment Issues"
   },
 
   {
     categoryName: "science",
     question: "question number 7",
-    options: ["Season 1", "Season 2", "Season 6", "Every Season"],
-    answer: "Every Season"
+    options: [ 
+      {id:1,text:"Season 3"},  
+      {id:2,text:"Season 5"},  
+      {id:3,text:"Season 7"},  
+      {id:4,text:"Season 4"}
+    ],
+    answer: "Season 5"
   },
   
   {
     categoryName: "science",
     question: "last question",
-    options: ["Season 3", "Season 5", "Season 7", "Season 4"],
+    options: [ 
+      {id:1,text:"Season 1"},  
+      {id:2,text:"Season 5"},  
+      {id:3,text:"Season 2"},  
+      {id:4,text:"Season 4"}
+    ],
     answer: "Season 4"
   }
   
