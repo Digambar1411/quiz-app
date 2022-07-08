0b736a93945c501c86e61200f031f01ecee4dbaa
Digambar <deshawaldigambar@gmail.com>
2022-07-08 10:41:18 +0530

    feat : added question page

--- src/frontend/pages/question-page/question-page.js
+++ src/frontend/pages/question-page/question-page.js
@@ -1,6 +1,56 @@
+import { Footer, Navbar} from "../../components/index";
+import { useNavigate } from "react-router-dom";
+import { useQuestion } from "../../contexts/index";
+import { useState } from "react";
+import "./question-page.css";
+
 export function QuestionPage(){
+    const [isActive, setIsActive] = useState(false);
+   
+    const optionHandler=(()=>setIsActive(!isActive));
+
+    const { navigate} = useNavigate();
+    const { nextQuestionHandler,curqueIndex,categoryQuestions, seconds, QuitQuiz} = useQuestion();
+       
     return (
-        <p>This is  Quiz page</p>
+        <>
+            <Navbar />
+            
+            <div className="question-area flex-col">
+                <div className="quiz-title">Quiz Category</div>
+                   
+                <div className="flex space-btwn">
+                    <div>Question : {curqueIndex + 1} / {categoryQuestions.length}</div>
+              
+                    <div className="flex center timer">
+                        <span className="material-symbols-outlined">
+                            timelapse
+                        </span>
+                        <div>00:{seconds<10? "0"+seconds : seconds}</div>
+                    </div>
+                </div>
+
+                <div className="question-section flex-col">
+                    <div className="question"> 
+                    {categoryQuestions[curqueIndex].questionText} </div>
+
+                    {
+                        categoryQuestions[curqueIndex].options.map(option=>{ const {id,text}=option;
+                            return (<ul className="options flex-col"><li className={ isActive ? "active-option" :"" }  key={id} onClick={()=>optionHandler}>{text}</li></ul>)
+                        })
+                    }
+
+                </div>
+
+                <div className="flex space-btwn">
+                    <button className="nav-btn" onClick={QuitQuiz}> Quit </button>
+
+                    <button className="nav-btn" onClick={nextQuestionHandler}> {"Next >"} </button>
+                </div>
+            </div>
+
+            <Footer />
+        </>
     )
    
 }
