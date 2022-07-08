ec396216b887cadc86d8b5963e01973085bba3d2
Digambar <deshawaldigambar@gmail.com>
2022-07-08 10:40:38 +0530

    feat : updated database questions

--- src/backend/db/quizzes.js
+++ src/backend/db/quizzes.js
@@ -3,98 +3,146 @@ import { v4 as uuid } from "uuid";
 export const quizzes = [
   {
     categoryName: "maths",
-    question: "question number 1",
+    questionText: "A triangle where all sides are congruent is called?",
     options: [
-      { id: uuid(),text:"narendra"},
-      { id: uuid(),text:"modi"},
-      { id: uuid(),text:"om"},
-      { id: uuid(),text:"birla"}
+      { id: uuid(),text:"Acute"},
+      { id: uuid(),text:"Right"},
+      { id: uuid(),text:"Equilateral"},
+      { id: uuid(),text:"Obtuse"}
     ],
-    answer: "om"
+    answer: "Equilateral"
   },
 
   {
     categoryName: "maths",
-    question: "question number 2",
+    questionText: "What is the area of a triangle with base 10 cm and height 25 cm?",
     options: [ 
-      {id:1,text:"Season 3"},  
-      {id:1,text:"Season 5"},  
-      {id:1,text:"Season 7"},  
-      {id:1,text:"Season 4"}
+      {id:1,text:"35 sq cm"},  
+      {id:1,text:"250 sq cm"},  
+      {id:1,text:"125 sq cm"},  
+      {id:1,text:"135 sq cm"}
     ],
-    answer: "Season 4",
+    answer: "125 sq cm",
   },
 
   {
     categoryName: "maths",
-    question: "question number 3",
+    questionText: "A triangle where all sides are congruent is called",
     options: [
-      {id:1,text:"Attachment Issues"}, 
-      {id:2,text:"Panic Attack"}, 
-      {id:3,text:"Trauma"}, 
+      {id:1,text:"Scalene"}, 
+      {id:2,text:"Isosceles"}, 
+      {id:3,text:"Equilateral"}, 
       {id:4,text:"Stress"}
     ],
     answer: "Attachment Issues",
   },
 
+  {
+    categoryName: "web developmemt",
+    questionText: "Choose the correct HTML element to define important text",
+    options: [
+      { id: uuid(),text:"<strong>"},
+      { id: uuid(),text:"<i>"},
+      { id: uuid(),text:"<important>"},
+      { id: uuid(),text:"<b>"}
+    ],
+    answer: "<strong>"
+  },
+
+  {
+    categoryName: "web developmemt",
+    questionText: "What will be the output of console.log(typeof typeof 1);",
+    options: [
+      { id: uuid(),text:"1"},
+      { id: uuid(),text:"string"},
+      { id: uuid(),text:"number"},
+      { id: uuid(),text:"true"}
+    ],
+    answer: "string"
+  },
+
+  {
+    categoryName: "web developmemt",
+    questionText: "What will be the output of console.log(('b' + 'a' + + 'a' + 'a').toLowerCase());",
+    options: [
+      { id: uuid(),text:"banana"},
+      { id: uuid(),text:"bananaa"},
+      { id: uuid(),text:"ba+aa"},
+      { id: uuid(),text:"option 4"}
+    ],
+    answer: "banana"
+  },
+
+ 
   {     
     categoryName: "physics",
-        question: "question number 4",
+    questionText: "The sky appears blue because of",
         options: [ 
-          {id:1,text:"Season 3"},  
-          {id:2,text:"Season 5"},  
-          {id:3,text:"Season 7"},  
-          {id:4,text:"option 4"}
+          {id:1,text:"Atmospheic water vapour"},  
+          {id:2,text:"Scattering of light"},  
+          {id:3,text:"Reflection on seawater"},  
+          {id:4,text:"Emission of blue wavelenght by the sun"}
         ],
-        answer: "option 4"
+        answer: "Scattering of light"
   },
 
   {
     categoryName: "physics",
-    question: "question number 5",
+    questionText: "Which of the below parts of sunlight makes solar cooker hot?",
     options: [ 
-      {id:1,text:"Season 3"},  
-      {id:2,text:"Season 5"},  
-      {id:3,text:"Season 7"},  
-      {id:4,text:"Season 4"}
+      {id:1,text:"Ultraviolet"},  
+      {id:2,text:"Red light"},  
+      {id:3,text:"Cosmic rays"},  
+      {id:4,text:"Infrared"}
     ],
-    answer: "Season 4"
+    answer: "Infrared"
   },
 
   {
     categoryName: "physics",
-    question: "question number 6",
+    questionText: "The speed of light, with the rise in the temperature of the medium",
     options: [
-      {id:1,text:"Attachment Issues"}, 
-      {id:2,text:"Panic Attack"}, 
-      {id:3,text:"Trauma"}, 
-      {id:4,text:"Stress"}
+      {id:1,text:"Suddenly drops"}, 
+      {id:2,text:"Remains unaltered"}, 
+      {id:3,text:"Decreases"}, 
+      {id:4,text:"Increases"}
     ],
-    answer: "Attachment Issues"
+    answer: "remains unaltered"
   },
 
   {
     categoryName: "science",
-    question: "question number 7",
+    questionText:"In fireworks, the green flame is produced because of",
     options: [ 
-      {id:1,text:"Season 3"},  
-      {id:2,text:"Season 5"},  
-      {id:3,text:"Season 7"},  
-      {id:4,text:"Season 4"}
+      {id:1,text:"Sodium"},  
+      {id:2,text:"Potassium"},  
+      {id:3,text:"Mercury"},  
+      {id:4,text:"Barium"}
     ],
-    answer: "Season 5"
+    answer: "Barium"
   },
   
   {
     categoryName: "science",
-    question: "last question",
+    questionText: "Heavy water is",
     options: [ 
-      {id:1,text:"Season 1"},  
-      {id:2,text:"Season 5"},  
-      {id:3,text:"Season 2"},  
-      {id:4,text:"Season 4"}
+      {id:1,text:"Deuterium oxide"},  
+      {id:2,text:"PH7"},  
+      {id:3,text:"Rain water"},  
+      {id:4,text:"Tritium oxide"}
     ],
-    answer: "Season 4"
+    answer: "Deuterium oxide"
+  },
+  {
+    categoryName: "science",
+    questionText: "Which of the following is used in pencils?",
+    options: [ 
+      {id:1,text:"Graphite"},  
+      {id:2,text:"Silicon"},  
+      {id:3,text:"Phosphorous"},  
+      {id:4,text:"Charcoal"}
+    ],
+    answer: "Graphite"
   }
   
 ];
