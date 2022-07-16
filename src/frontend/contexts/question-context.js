94a7ae373df34ac81b80e6216fe5916ac0b5d6e1
Digambar <deshawaldigambar@gmail.com>
2022-07-16 14:51:34 +0530

    feat : added optinHandler function

--- src/frontend/contexts/question-context.js
+++ src/frontend/contexts/question-context.js
@@ -1,21 +1,25 @@
 import { useState, useContext, createContext, useEffect} from "react";
 import axios from "axios";
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 
 const QuestionContext = createContext();
 const useQuestion = () => useContext(QuestionContext);
 
 function QuestionProvider({children}){
     const navigate = useNavigate();
-    
+        
     const initialTimerValue = 30;
-    const initialIndexValue=0
-    const initialQuestions = useEffect(()=>{
-        getData();
-    
-    },[])
+ 
+    const initialIndexValue=0;
+    const initialScore=0;
+    const initialQuestions = useEffect(()=>{getData()},[]);
 
     const [seconds, setSeconds]= useState(initialTimerValue);
+    const [score, setScore] = useState(initialScore);
+    const [selectedOption, setSelectedOption]= useState([]);
+    const [category, setCategory]= useState("");
+
     const [questions, setQuestions] = useState([initialQuestions]);
     const [curqueIndex, setCurqueIndex] = useState(initialIndexValue); 
     const [categoryQuestions, setCategoryQuestions] = useState([]);
@@ -35,14 +39,16 @@ function QuestionProvider({children}){
     const getData = async ()=>{
         const response =  await axios.get("api/quizzes");
         setQuestions(response.data.quizes);
-        console.log(response.data.quizes);
     }
 
     const getSelectedCategoryQuestions = (name)=>{
+        setCategory(name)
+        setScore(initialScore);
         setCurqueIndex(initialIndexValue);  //if curqueIndex is not reseted then for other category, quiz will start from last categories current index question. hence reseting index to 0
         navigate("/rules");
         const getCategoryQuestions = ()=> questions.filter(question=>question.categoryName===name.toLowerCase())
         setCategoryQuestions(getCategoryQuestions());
+        setSelectedOption([]);
     }
             
     const nextQuestionHandler = ()=>{
@@ -61,7 +67,16 @@ function QuestionProvider({children}){
         navigate("/")
     }
 
-    return <QuestionContext.Provider value= {{seconds,questions, curqueIndex, nextQuestionHandler, QuitQuiz, getSelectedCategoryQuestions, categoryQuestions}} >{children}
+    const selectOptionHandler = (entireOption)=>{
+        if(entireOption.isCorrect){
+            setScore(score+1);
+           
+        }
+        
+        setSelectedOption([...selectedOption,entireOption]);
+    }
+
+    return <QuestionContext.Provider value= {{category, score, seconds, questions, curqueIndex, selectedOption, categoryQuestions, selectOptionHandler, nextQuestionHandler, QuitQuiz, getSelectedCategoryQuestions}} >{children}
     </QuestionContext.Provider>
 }
 
