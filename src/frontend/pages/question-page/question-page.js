969bb31a5030a57d0a415d3dfd402aefdd5371e5
Digambar <deshawaldigambar@gmail.com>
2022-07-16 14:55:18 +0530

    style: used button for options

--- src/frontend/pages/question-page/question-page.js
+++ src/frontend/pages/question-page/question-page.js
@@ -1,23 +1,20 @@
-import { Footer, Navbar} from "../../components/index";
-import { useNavigate } from "react-router-dom";
-import { useQuestion } from "../../contexts/index";
 import { useState } from "react";
+import { Footer, Navbar} from "../../components/index";
+import { useQuestion } from "../../contexts/index";
 import "./question-page.css";
 
 export function QuestionPage(){
-    const [isActive, setIsActive] = useState(false);
-   
-    const optionHandler=(()=>setIsActive(!isActive));
 
-    const { navigate} = useNavigate();
-    const { nextQuestionHandler,curqueIndex,categoryQuestions, seconds, QuitQuiz} = useQuestion();
-       
+    const [isActive, setIsActive] = useState(false);
+    const { nextQuestionHandler, category, categoryQuestions, seconds, 
+        QuitQuiz, curqueIndex,  selectOptionHandler} = useQuestion();
+  
     return (
         <>
             <Navbar />
             
             <div className="question-area flex-col">
-                <div className="quiz-title">Quiz Category</div>
+                <div className="quiz-title">{ category }</div>
                    
                 <div className="flex space-btwn">
                     <div>Question : {curqueIndex + 1} / {categoryQuestions.length}</div>
@@ -32,17 +29,23 @@ export function QuestionPage(){
 
                 <div className="question-section flex-col">
                     <div className="question"> 
-                    {categoryQuestions[curqueIndex].questionText} </div>
-
+                        {categoryQuestions[curqueIndex].questionText} 
+                    </div>
                     {
-                        categoryQuestions[curqueIndex].options.map(option=>{ const {id,text}=option;
-                            return (<ul className="options flex-col"><li className={ isActive ? "active-option" :"" }  key={id} onClick={()=>optionHandler}>{text}</li></ul>)
+                        categoryQuestions[curqueIndex].options.map((option)=>{ const {id,text,isCorrect}=option;
+                            return (
+                            <div className="options flex-col">
+                                <button 
+                                    className={ isActive ? "active-option" :"" }  
+                                    key={id} 
+                                    onClick={()=>selectOptionHandler(option)}>{text}
+                                </button>
+                            </div>)
                         })
                     }
-
                 </div>
 
-                <div className="flex space-btwn">
+                <div className="controls">
                     <button className="nav-btn" onClick={QuitQuiz}> Quit </button>
 
                     <button className="nav-btn" onClick={nextQuestionHandler}> {"Next >"} </button>
