247b44b4ab34e286a32c54c4605a239248a99a4c
Digambar <deshawaldigambar@gmail.com>
2022-07-17 18:57:56 +0530

    style : changes in some css class name

--- src/frontend/pages/question-page/question-page.js
+++ src/frontend/pages/question-page/question-page.js
@@ -13,11 +13,11 @@ export function QuestionPage(){
             <div className="question-area flex-col">
                 <div className="quiz-title">{ category }</div>
                    
-                <div className="flex space-btwn">
-                    <div>Question : {curqueIndex + 1} / {categoryQuestions.length}</div>
+                <div className="controls">
+                    <div>Question : 0{curqueIndex + 1} / 0{categoryQuestions.length}</div>
               
                     <div className="flex center timer">
-                        <span className="material-symbols-outlined">
+                        <span className="material-symbols-outlined timer-icon">
                             timelapse
                         </span>
                         <div>00:{seconds<10? "0"+seconds : seconds}</div>
@@ -46,8 +46,8 @@ export function QuestionPage(){
                 </div>
 
                 <div className="controls">
-                    <button className="nav-btn" onClick={QuitQuiz}> Quit </button>
-                    <button className="nav-btn" onClick={categoryQuestions.length===curqueIndex+1 ? viewResult : nextQuestionHandler}> {categoryQuestions.length===curqueIndex+1 ? "Result": "Next >"} 
+                    <button className="control-btn" onClick={QuitQuiz}> Quit </button>
+                    <button className="control-btn" onClick={categoryQuestions.length===curqueIndex+1 ? viewResult : nextQuestionHandler}> {categoryQuestions.length===curqueIndex+1 ? "Result": "Next >"} 
                     </button>
                 </div>
             </div>
