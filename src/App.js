5049582173061286fdb1b0bd81ed0f15bf9e9e15
Digambar <deshawaldigambar@gmail.com>
2022-07-18 22:50:59 +0530

    feat : added routes for auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import {Routes, Route } from "react-router-dom";
-import { Home, Result, QuestionPage, Rule} from "./frontend/pages"
+import { Home, Result, QuestionPage, Rule, Login, Signup} from "./frontend/pages"
 
 
 
@@ -11,6 +11,8 @@ function App() {
       <Route path="/question" element={<QuestionPage />} />
       <Route path="/result" element={<Result />} />
       <Route path ="/rules" element={<Rule />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   )
 }
--- src/frontend/pages/index.js
+++ src/frontend/pages/index.js
@@ -2,3 +2,5 @@ export { Home } from "./home/home";
 export { Result } from "./result/result";
 export { QuestionPage } from "./question-page/question-page";
 export { Rule} from "./rules/rule";
+export { Login } from "./Auth/login"
+export { Signup} from "./Auth/signup"
